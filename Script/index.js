e4603b21d7094cddc49b3abe9c3708faad25eb7f
nataliaortizvi <natiorvi00@gmail.com>
2021-05-31 01:20:33 -0500

    cambios hashtags

--- Script/index.js
+++ Script/index.js
@@ -2,8 +2,6 @@ const productForm = document.querySelector('.write');
 const write__change = document.querySelector('.write__change');
 const write__btn = document.querySelector('.write__btn');
 
-let contador = 0;
-
 productForm.addEventListener('submit', function (event) {
     event.preventDefault();
     //contador ++;
@@ -20,14 +18,78 @@ productForm.addEventListener('submit', function (event) {
     })
 });
 
-
-
 write__change.addEventListener('click', function() {
     window.location.href='./mapa.html';
 });
 
 
+const hashtag__one = document.querySelector('.hashtag__one');
+const hashtag__two = document.querySelector('.hashtag__two');
+const hashtag__three = document.querySelector('.hashtag__three');
+const hashtag__four = document.querySelector('.hashtag__four');
+const hashtag__five = document.querySelector('.hashtag__five');
+const hashtag__six = document.querySelector('.hashtag__six');
 
+let changes = 0;
+
+function handleChanges () {
+    changes++;
+    
+    if(changes == 1){
+        hashtag__one.style.display = 'none';
+        hashtag__two.style.display = 'block';
+        hashtag__three.style.display = 'none';
+        hashtag__four.style.display = 'none';
+        hashtag__five.style.display = 'none';
+        hashtag__six.style.display = 'none';
+    }
+    
+    if(changes == 2){
+        hashtag__one.style.display = 'none';
+        hashtag__two.style.display = 'none';
+        hashtag__three.style.display = 'block';
+        hashtag__four.style.display = 'none';
+        hashtag__five.style.display = 'none';
+        hashtag__six.style.display = 'none';
+    }
+    
+    if(changes == 3) {
+        hashtag__one.style.display = 'none';
+        hashtag__two.style.display = 'none';
+        hashtag__three.style.display = 'none';
+        hashtag__four.style.display = 'block';
+        hashtag__five.style.display = 'none';
+        hashtag__six.style.display = 'none';
+    }
+
+    if(changes == 4) {
+        hashtag__one.style.display = 'none';
+        hashtag__two.style.display = 'none';
+        hashtag__three.style.display = 'none';
+        hashtag__four.style.display = 'none';
+        hashtag__five.style.display = 'block';
+        hashtag__six.style.display = 'none';
+    }
+
+    if(changes >= 5) {
+        changes = 0;
+        hashtag__one.style.display = 'none';
+        hashtag__two.style.display = 'none';
+        hashtag__three.style.display = 'none';
+        hashtag__four.style.display = 'none';
+        hashtag__five.style.display = 'none';
+        hashtag__six.style.display = 'block';
+    }
+}
+
+setInterval(handleChanges, 2000);
+
+
+
+
+
+
+///////////////////////////////P5.JS///////////////////////////////
 
 let one;
 
@@ -44,8 +106,6 @@ function draw () {
     noStroke(); 
     one.pintar();
 
-    /*fill(210);
-    textSize (40);
-    text('#MeDuelesColombia', 550, 200);*/
+    console.log(changes);
   
 }
